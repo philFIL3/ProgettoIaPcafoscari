6ddba84aa00348eff3c786a2bf5d94b448aa9a17
checky98 <136791962+checky98@users.noreply.github.com>
2024-11-09 17:55:29 +0100

    Update suits.c

--- suits.c
+++ suits.c
@@ -2,19 +2,42 @@
 #include <stdio.h>
 #include <wchar.h>
 
-int main(int argc, char* argv[])
+int suits(int argc, char* argv[])
 {
         setlocale(LC_ALL, "");
 
-        wchar_t *clubs = L"\u2663  \u2667"; //la prima da il colore pieno, la seconda solo il contorno
+        wchar_t *clubs = L"\u2663  \u2667";
         wchar_t *diamonds = L"\u2666  \u2662";
         wchar_t *hearts = L"\u2665  \u2661";
         wchar_t *spades = L"\u2660  \u2664";
 
-        printf("%ls\n", clubs);
-        printf("%ls\n", diamonds);
-        printf("%ls\n", hearts);
-        printf("%ls\n", spades);
+        printf("%ls\n", clubs);         // fiori
+        printf("%ls\n", diamonds);      // quadri
+        printf("%ls\n", hearts);        // cuori
+        printf("%ls\n", spades);        // picche
 
+        return 0;
+}
+
+int semi(int argc, char* argv[]) {
+        
+        setlocale (LC_ALL, "");
+
+        wchar_t *cuori = L"\u2665";
+        wchar_t *quadri = L"\u2666";
+        wchar_t *picche = L"\u2660"; 
+        wchar_t *fiori = L"\u2663";
+
+        printf("%ls\n", cuori);
+        printf("%ls\n", quadri);
+        printf("%ls\n", picche);
+        printf("%ls\n", fiori); 
+        
+        return 0;
+}
+
+int main (int argc, char* argv[]){
+
+        semi(argc, argv);
         return 0;
 }
