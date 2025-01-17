5a806fc843380c05ca258a43f8868c864536ba5d
Melania4 <899296@stud.unive.it>
2025-01-17 16:25:27 +0100

    modifiche in commento alla fine del codice

--- progettocarte.c
+++ progettocarte.c
@@ -22,12 +22,12 @@ void new_mazzo(Carte * const Mazzo_iterato, const char * Array_numero_carta[], c
 void mischia_mazzo(Carte * const Mazzo_iterato);
 void distribuisci(const Carte * const Mazzo_iterato, int numero_giocatori, int punti_vita[], int *punti_sul_campo, int prima_mano, Carte carte_scoperte[]);   
 int scegli_giocatore(int numero_giocatori); 
+int prossimo_giocatore (int turno_corrente, int punti_vita[], int numero_giocatori);
 void effetto_carte (const Carte carta, int giocatore_corrente, int numero_giocatori, int punti_vita[], int *punti_sul_campo);
-void fasi(Carte *const mazzo, int numero_giocatori, int punti_vita[], int *punti_sul_campo, Carte carte_scoperte[]);
+void fasi(Carte *const mazzo, int numero_giocatori, int punti_vita[], int *punti_sul_campo, Carte carte_scoperte[], const char *numero_carta[], const char *seme[]);
 
 int main(void)
 {
-	// Carte mazzo[MAZZOCARTE]; // definisce l'array del mazzo
     Carte *mazzo = malloc(MAZZOCARTE * sizeof(Carte));
     if(mazzo == NULL) {
         return 1;
@@ -68,9 +68,17 @@ int main(void)
     
     int punti_sul_campo=0;  // tiene conto dei punti sul campo
     Carte carte_scoperte[MAXGIOCATORI];  // salva le carte scoperte
+
+    // controllo se ho abbastanza carte
+    if(numero_giocatori*2 > MAZZOCARTE) {
+        printf("Errore: non ci sono abbastanza carte!!");
+        free(mazzo);
+        return 1;
+    }
+
     distribuisci(mazzo, numero_giocatori, punti_vita, &punti_sul_campo, 1, carte_scoperte); // distribuisce le carte
 
-    fasi(mazzo, numero_giocatori, punti_vita, &punti_sul_campo, carte_scoperte);  // inizia le fasi
+    fasi(mazzo, numero_giocatori, punti_vita, &punti_sul_campo, carte_scoperte, numero_carta, seme);  // inizia le fasi
 
     free(mazzo);
     mazzo=NULL; // azzera il puntatore
@@ -82,16 +90,25 @@ int main(void)
 void new_mazzo(Carte * const Mazzo_iterato, const char * Array_numero_carta[],
                const char * Array_Seme[])
 {
-	// size_t nuovo_mazzo;
+    // controllo puntatore NULL
+    if(Mazzo_iterato==NULL || Array_numero_carta==NULL || Array_Seme==NULL) {
+        printf("Errore: passato un puntatore NULL!!");
+        return;
+    }
+
 	for (size_t mazzo_nuovo = 0; mazzo_nuovo < MAZZOCARTE; ++mazzo_nuovo) {
 		Mazzo_iterato[mazzo_nuovo].numero_carta = Array_numero_carta[mazzo_nuovo % NUMEROCARTA];
 		Mazzo_iterato[mazzo_nuovo].seme = Array_Seme[mazzo_nuovo / NUMEROCARTA];
-// Mazzo_iterato[mazzo_nuovo] accede agli elementi di .seme e .numero_carta
 	}
 }
 // mischia le carte
 void mischia_mazzo(Carte * const Mazzo_iterato)
 {
+    // controllo puntatore NULL
+    if(Mazzo_iterato==NULL) {
+        printf("Errore: passato un puntatore NULL!!");
+        return;
+    }
 	for (size_t i = 0; i < MAZZOCARTE; ++i) {
 		size_t j = rand() % MAZZOCARTE ;
 		Carte randomico = Mazzo_iterato[i];
@@ -103,6 +120,11 @@ void mischia_mazzo(Carte * const Mazzo_iterato)
 // distribuisci le carte
 void distribuisci(const Carte * const Mazzo_iterato, int numero_giocatori, int punti_vita[], int *punti_sul_campo, int prima_mano, Carte carte_scoperte[])
 {
+    // controllo puntatore NULL
+    if(Mazzo_iterato==NULL || punti_vita==NULL || punti_sul_campo==NULL || carte_scoperte==0) {
+        printf("Errore: passato un puntatore NULL!!");
+        return;
+    }
     int carte_distribuite = 0;
 
     for (int i = 0; i < numero_giocatori; ++i){
@@ -132,20 +154,28 @@ void distribuisci(const Carte * const Mazzo_iterato, int numero_giocatori, int p
 // applica l'effetto delle carte
 void effetto_carte(const Carte carta, int giocatore_corrente, int numero_giocatori, int punti_vita[], int *punti_sul_campo)
 {
+    // controllo puntatore NULL
+    if(punti_vita==NULL || punti_sul_campo==NULL) {
+        printf("Errore: passato un puntatore NULL!!");
+        return;
+    }
+
     if (strcmp(carta.numero_carta, "Due") == 0 || strcmp(carta.numero_carta, "Tre") == 0 || strcmp(carta.numero_carta, "Quattro") == 0 || strcmp(carta.numero_carta, "Cinque") == 0 || strcmp(carta.numero_carta, "Sei") == 0) {
         printf("Non succede nulla\n");
         return;
     }
     
     if (strcmp(carta.numero_carta, "Sette") == 0) {
-        int giocatore_successivo = (giocatore_corrente+1) % numero_giocatori;        
+        // int giocatore_successivo = (giocatore_corrente+1) % numero_giocatori;       
+        int giocatore_successivo = prossimo_giocatore(giocatore_corrente, punti_vita, numero_giocatori); 
         printf("Il giocatore %d forza il giocatore %d a scoprire ed applicare l'effetto della sua carta coperta\n", giocatore_corrente+1, giocatore_successivo+1);
     } else if (strcmp(carta.numero_carta, "Jack") == 0) {
         int giocatore_precedente = (giocatore_corrente-1+numero_giocatori) % numero_giocatori;
         punti_vita[giocatore_precedente]++;
         printf("Il giocatore %d dà 1 punto vita al giocatore %d\n", giocatore_corrente+1, giocatore_precedente+1);
     } else if (strcmp(carta.numero_carta, "Regina") == 0) {
-        int giocatore_successivo = (giocatore_corrente+2) % numero_giocatori;
+        // int giocatore_successivo = (giocatore_corrente+2) % numero_giocatori;
+        int giocatore_successivo = prossimo_giocatore(giocatore_corrente, punti_vita, numero_giocatori); 
         punti_vita[giocatore_successivo]++;
         printf("Il giocatore %d dà 1 punto vita al giocatore %d\n", giocatore_corrente+1, giocatore_successivo+1);
     } else if (strcmp(carta.numero_carta, "Asso") == 0) {
@@ -169,14 +199,27 @@ void effetto_carte(const Carte carta, int giocatore_corrente, int numero_giocato
 }
 
 // scegli il primo giocatore random
-//TO DO: ALGORITMO PER I TURNI
 int scegli_giocatore(int numero_giocatori) {
     return rand() % numero_giocatori;
 }
 
+// sceglie il prossimo giocatore
+int prossimo_giocatore(int turno_corrente, int punti_vita[], int numero_giocatori) {
+    do {
+        turno_corrente = (turno_corrente+1)%numero_giocatori;
+    } while (punti_vita[turno_corrente]<=0);
+    return turno_corrente;
+}
+
 // definisce le fasi del gioco
-void fasi(Carte *const mazzo, int numero_giocatori, int punti_vita[], int *punti_sul_campo, Carte carte_scoperte[]) 
+void fasi(Carte *const mazzo, int numero_giocatori, int punti_vita[], int *punti_sul_campo, Carte carte_scoperte[], const char *numero_carta[], const char *seme[]) 
 {
+    // controllo puntatore NULL
+    if(mazzo==NULL || punti_vita==NULL || punti_sul_campo==NULL || carte_scoperte==NULL) {
+        printf("Errore: passato un puntatore NULL!!");
+        return;
+    }
+    
     int turno_corrente = 0;
     int fase_corrente = 1;
     int giocatori_attivi = numero_giocatori;
@@ -187,12 +230,11 @@ void fasi(Carte *const mazzo, int numero_giocatori, int punti_vita[], int *punti
         printf("\n\nFase %d\n", fase_corrente);
 
         for(int i=0; i<numero_giocatori; i++) {
-            // passa al giocatore successivo
+            // passa al giocatore successivo se quello corrente è eliminato
             while(punti_vita[turno_corrente]<=0) {
                 turno_corrente = (turno_corrente+1)%numero_giocatori;
             }
             printf("\nTurno del giocatore %d\n", turno_corrente+1);
-
             printf("Il giocatore %d applica l'effetto della carta scoperta: %s di %s\n", turno_corrente+1, carte_scoperte[turno_corrente].numero_carta, carte_scoperte[turno_corrente].seme);
             effetto_carte(carte_scoperte[turno_corrente], turno_corrente, numero_giocatori, punti_vita, punti_sul_campo);
 
@@ -206,7 +248,7 @@ void fasi(Carte *const mazzo, int numero_giocatori, int punti_vita[], int *punti
 
             printf("La tua carta coperta è: %s di %s\n", mazzo[turno_corrente*2].numero_carta, mazzo[turno_corrente*2].seme);
             printf("Vuoi scoprire la tua carta coperta? ");
-            char scelta[3];
+            char scelta[10];
             scanf("%2s", scelta);
 
             if(strcmp(scelta, "si")==0 || strcmp(scelta, "Si")==0 || strcmp(scelta, "sì")==0 || strcmp(scelta, "Sì")==0) {
@@ -218,24 +260,49 @@ void fasi(Carte *const mazzo, int numero_giocatori, int punti_vita[], int *punti
                 printf("Scelta non valida\n");
             }
 
-            do {
+           // verifico se la fase deve finire dopo aver applicato l'effetto delle carte
+           if(giocatori_attivi<=1) {
+            printf("\nFine della fase %d, c'è un solo giocatore rimanente!\n", fase_corrente);
+            break;
+           }
+
+           do {
                 turno_corrente=(turno_corrente+1)%numero_giocatori;
-            } while (punti_vita[turno_corrente]<=0);
-            
-            // controlla se ci sono dei giocatori di eliminare alla fine della fase
-            printf("\nFine della fase %d\n", fase_corrente);
-            giocatori_attivi=0;
-            for(int i=0; i<numero_giocatori; i++) {
-                if(punti_vita[i]>0) {
-                    printf("Il giocatore %d ha ancora %d punti vita\n", i+1, punti_vita[i]);
-                    giocatori_attivi++;
-                } else {
-                    printf("Il giocatore %d ha terminato i punti vita ed è eliminato\n", i+1);
-                }
+            } while (punti_vita[turno_corrente]<=0);  
+        }
+
+        if(giocatori_attivi<=1) {
+            break;
+        }
+
+        printf("\nFine della fase %d\n", fase_corrente);
+        fase_corrente++;
+        
+        // controlla se ci sono dei giocatori di eliminare alla fine della fase
+        giocatori_attivi=0;
+        for(int i=0; i<numero_giocatori; i++) {
+            if(punti_vita[i]>0) {
+                printf("Il giocatore %d ha ancora %d punti vita\n", i+1, punti_vita[i]);                    
+                giocatori_attivi++;
+            } else {
+                printf("Il giocatore %d ha terminato i punti vita ed è eliminato\n", i+1);
             }
         }
-        fase_corrente++;
+        /*
+        // mischio e ridistribuisco le carte a fine fase
+        printf("\nRitiro carte\n");
+        new_mazzo(mazzo, numero_carta, seme);
+        mischia_mazzo(mazzo);
+        distribuisci(mazzo, numero_giocatori, punti_vita, punti_sul_campo, 0, carte_scoperte);
+
+        // Mostra le nuove carte distribuite a ciascun giocatore
+        printf("\nNuovo tavolo di gioco:\n");
+        for(int i = 0; i < numero_giocatori; i++) {
+            printf("Giocatore %d: Carta coperta: %s di %s\n", i + 1, mazzo[i * 2].numero_carta, mazzo[i * 2].seme);
+            printf("Giocatore %d: Carta scoperta: %s di %s\n", i + 1, mazzo[i * 2 + 1].numero_carta, mazzo[i * 2 + 1].seme);
+        } */
     }
+
     printf("\nPartita terminata!!\n");
     for(int i=0; i<numero_giocatori; i++) {
         if(punti_vita[i]>0) {
@@ -244,3 +311,13 @@ void fasi(Carte *const mazzo, int numero_giocatori, int punti_vita[], int *punti
         }
     }
 }
+
+/*TO DO: -- quando un giocatore viene eliminato, il turno successivo deve essere da un giocatore in meno rispetto a quelli iniziali, 
+        -- sistemare ritiro e ridistribuzione delle carte alla fine di ogni fase con visualizzazione del nuovo campo di gioco (in commento da riga 292 se volete usare quella se no cambiate pure)
+        -- implementare effetto a catena quando un giocatore obbliga un altro a scoprire e applicare l'effetto della sua carta coperta
+        -- controllare effetto carta Q in due persone come scritto nel documento del progetto 
+        -- controllare se ci sono bug vari e se sono state fatte tutte le implementazioni minime necessarie richieste dal prof*/
+
+/*MODIFICHE: -- aggiunto una funzione per determinare il prossimo giocatore negli effetti delle carte 
+            -- aggiunto un controllo per vedere se ci sono abbastanza carte per tutti i giocatori (anche se forse non è necessario)
+            -- aggiunto dei controlli nelle funzioni per essere sicuri che i puntatori passati alle funzioni siano validi*/
